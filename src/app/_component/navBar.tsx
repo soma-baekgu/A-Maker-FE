f56cac0416582793edd35f04c8da3f8cfa51af48
nohy6630 <nohy6630@gmail.com>
2024-07-03 21:59:42 +0900

    feat: 워크스페이스 목록 ui 구현

--- src/app/_component/navBar.tsx
+++ src/app/_component/navBar.tsx
@@ -1,21 +1,29 @@
+'use client';
 import styles from './navBar.module.css';
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export default function NavBar() {
+    const pathName = usePathname();
+
     return (
-        <div className={styles.navBar}>
-            <Link href="/main" className={styles.navButton}>
-                <div className={styles.buttonIcon}></div>
-            </Link>
-            <Link href="/chatting" className={styles.navButton}>
-                <div className={styles.buttonIcon}></div>
-            </Link>
-            <Link href="/dm" className={styles.navButton}>
-                <div className={styles.buttonIcon}></div>
-            </Link>
-            <Link href="/task" className={styles.navButton}>
-                <div className={styles.buttonIcon}></div>
-            </Link>
-        </div>
+        <>
+            {pathName !== '/workspaces' &&
+                <div className={styles.navBar}>
+                    <Link href="/main" className={styles.navButton}>
+                        <div className={styles.buttonIcon}></div>
+                    </Link>
+                    <Link href="/chatting" className={styles.navButton}>
+                        <div className={styles.buttonIcon}></div>
+                    </Link>
+                    <Link href="/dm" className={styles.navButton}>
+                        <div className={styles.buttonIcon}></div>
+                    </Link>
+                    <Link href="/task" className={styles.navButton}>
+                        <div className={styles.buttonIcon}></div>
+                    </Link>
+                </div>
+            }
+        </>
     )
 };
--- src/app/_component/topBar.tsx
+++ src/app/_component/topBar.tsx
@@ -1,17 +1,28 @@
+'use client';
 import styles from './topBar.module.css';
+import {usePathname} from "next/navigation";
+import Link from "next/link";
 
 type Props = {
     workspaceName: string
 }
 
 export default function TopBar({workspaceName}: Props) {
+    const pathName = usePathname();
+
     return (
-        <div className={styles.topBar}>
-            <div className={styles.topBarButton}></div>
-            <div className={styles.workspaceTitle}>
-                {workspaceName}
-            </div>
-            <div className={styles.topBarButton}></div>
-        </div>
+        <>
+            {pathName !== '/workspaces' &&
+                <div className={styles.topBar}>
+                    <Link href="/workspaces">
+                        <div className={styles.topBarButton}></div>
+                    </Link>
+                    <div className={styles.workspaceTitle}>
+                        {workspaceName}
+                    </div>
+                    <div className={styles.topBarButton}></div>
+                </div>
+            }
+        </>
     )
 };
