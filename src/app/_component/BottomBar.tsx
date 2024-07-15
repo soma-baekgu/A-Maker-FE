6bf90607c437c186f16d30cb4762a2a277c768fc
nohy6630 <nohy6630@gmail.com>
2024-07-15 16:51:57 +0900

    feat: 상단바 component 구현

--- src/app/_component/BottomBar.tsx
+++ src/app/_component/BottomBar.tsx
@@ -7,13 +7,13 @@ export default function BottomBar() {
         <>
             <div className={styles.component}>
                 <Link href="/home">
-                    <Image src="/button/home.png" alt="home" width={60} height={60} />
+                    <Image className={styles.button} src="/button/home.png" alt="home" width={60} height={60} />
                 </Link>
                 <Link href="/chat">
-                    <Image src="/button/chat.png" alt="chat" width={60} height={60} />
+                    <Image className={styles.button} src="/button/chat.png" alt="chat" width={60} height={60} />
                 </Link>
                 <Link href="/alarm">
-                    <Image src="/button/alarm.png" alt="alarm" width={60} height={60} />
+                    <Image className={styles.button} src="/button/alarm.png" alt="alarm" width={60} height={60} />
                 </Link>
             </div>
         </>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,33 @@
+"use client";
+
 import styles from './layout.module.css';
 import Link from "next/link";
-import TopBar from "@/app/_component/topBar";
+import TopBar from "@/app/_component/TopBar";
 import NavBar from "@/app/_component/navBar";
 import BottomBar from "@/app/_component/BottomBar";
+import {usePathname} from "next/navigation";
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
-    const workspaceName = "캡스톤 디자인 A조";
+    const pathname = usePathname();
+    let pageType;
+
+    if (pathname === '/home') {
+        pageType = '홈';
+    } else if (pathname === '/chat') {
+        pageType = '채팅';
+    } else if (pathname === '/alarm') {
+        pageType = '알림';
+    } else if (pathname === '/setting') {
+        pageType = '설정';
+    } else {
+        pageType = '기타';
+    }
 
     return (
         <html lang="en">
         <body className={styles.body}>
-        <div className>
-            <TopBar workspaceName={workspaceName}/>
-            <div className={styles.content}>{children}</div>
-        </div>
+        <TopBar pageType={pageType}/>
+        <div className={styles.content}>{children}</div>
         <BottomBar/>
         </body>
         </html>
