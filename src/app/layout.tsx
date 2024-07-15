ac2704941ec0b18a98b9229d9f4ff13d6327f8aa
nohy6630 <nohy6630@gmail.com>
2024-07-15 16:14:05 +0900

    feat: 하단바 component 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import styles from './layout.module.css';
 import Link from "next/link";
 import TopBar from "@/app/_component/topBar";
 import NavBar from "@/app/_component/navBar";
+import BottomBar from "@/app/_component/BottomBar";
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
     const workspaceName = "캡스톤 디자인 A조";
@@ -9,11 +10,11 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     return (
         <html lang="en">
         <body className={styles.body}>
-        <div className={styles.main}>
+        <div className>
             <TopBar workspaceName={workspaceName}/>
             <div className={styles.content}>{children}</div>
         </div>
-        <NavBar/>
+        <BottomBar/>
         </body>
         </html>
     );
