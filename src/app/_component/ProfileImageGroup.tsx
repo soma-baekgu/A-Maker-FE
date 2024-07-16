e0b634142e55341d5a86f04d1d67dd68b4ec6275
nohy6630 <nohy6630@gmail.com>
2024-07-16 15:18:05 +0900

    feat: 채팅방 컴포넌트 생성

---
+++ src/app/_component/ProfileImageGroup.tsx
@@ -0,0 +1,42 @@
+import styles from './profileImageGroup.module.css';
+
+type Props = {
+    imageUrls: string[]
+}
+
+export default function ProfileImageGroup({imageUrls}: Props) {
+    const cnt = imageUrls.length;
+
+    return (
+        <div className={styles.component}>
+            <div className={styles.profileImages}>
+                {
+                    cnt >= 1
+                    &&
+                    <div className={styles.profileImage}>
+                    </div>
+                }
+                {
+                    cnt >= 3
+                    &&
+                    <div className={styles.profileImage}>
+                    </div>
+                }
+            </div>
+            <div className={styles.profileImages}>
+                {
+                    cnt >= 2
+                    &&
+                    <div className={styles.profileImage}>
+                    </div>
+                }
+                {
+                    cnt >= 4
+                    &&
+                    <div className={styles.profileImage}>
+                    </div>
+                }
+            </div>
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import styles from './layout.module.css';
-import Link from "next/link";
-import TopBar from "@/app/_component/TopBar";
-import NavBar from "@/app/_component/navBar";
 import BottomBar from "@/app/_component/BottomBar";
-import {usePathname} from "next/navigation";
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
