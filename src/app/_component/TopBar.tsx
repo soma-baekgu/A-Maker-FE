7896506178936d835fb23ede041e044aa31f1849
nohy6630 <nohy6630@gmail.com>
2024-07-17 14:24:58 +0900

    feat: 채팅방 생성 모달 UI 구현

--- src/app/_component/TopBar.tsx
+++ src/app/_component/TopBar.tsx
@@ -5,17 +5,21 @@ import Image from "next/image";
 
 type Props = {
     pageType: string
+    onCreateChat?: () => void
+    onSearchChat?: () => void
 }
 
-export default function TopBar({pageType}: Props) {
+export default function TopBar({pageType, onCreateChat, onSearchChat}: Props) {
     return (
         <div className={styles.component}>
             <div className={styles.workspace}></div>
             <div className={styles.description}>{pageType}</div>
-            <Image className={styles.button} src="/button/createChat.png" alt="createChat" width={60} height={60} />
-            <Image className={styles.button} src="/button/searchChat.png" alt="searchChat" width={60} height={60} />
+            {onCreateChat &&
+                <Image className={styles.button} src="/button/createChat.png" alt="createChat" width={60} height={60} onClick={onCreateChat}/>}
+            {onSearchChat &&
+                <Image className={styles.button} src="/button/searchChat.png" alt="searchChat" width={60} height={60} onClick={onSearchChat}/>}
             <Link href="/setting">
-                <Image className={styles.button} src="/button/setting.png" alt="setting" width={60} height={60} />
+                <Image className={styles.button} src="/button/setting.png" alt="setting" width={60} height={60}/>
             </Link>
         </div>
     )
