ba2d59b23093c058409637f1a111887f90cd5b49
nohy6630 <nohy6630@gmail.com>
2024-07-21 23:41:19 +0900

    feat: 채팅방 생성 api 연동

--- src/app/chat/_component/CreateChatModal.tsx
+++ src/app/chat/_component/CreateChatModal.tsx
@@ -1,12 +1,28 @@
+import {useState} from "react";
 import styles from './createChatModal.module.css';
 import Image from "next/image";
 
 type Props = {
     setVisible: (visible: boolean) => void,
+    createChatRoom: (chatroomName: string) => void,
 }
-export default function CreateChatModal({setVisible}: Props) {
+
+export default function CreateChatModal({setVisible, createChatRoom}: Props) {
+    const [inputValue, setInputValue] = useState("");
+
     const handleClose = () => {
         setVisible(false);
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleCreateChat = () => {
+        createChatRoom(inputValue);
+        setInputValue("");
+        setVisible(false);
+        location.reload()
     }
 
     return (
@@ -19,9 +35,9 @@ export default function CreateChatModal({setVisible}: Props) {
                 </div>
                 <div className={styles.inputSection}>
                     <div className={styles.inputGuide}>채팅방 이름을 입력해주세요.</div>
-                    <input className={styles.input}></input>
+                    <input className={styles.input} value={inputValue} onChange={handleInputChange}></input>
                 </div>
-                <div className={styles.button} onClick={handleClose}>생성</div>
+                <div className={styles.button} onClick={handleCreateChat}>생성</div>
             </div>
         </div>
     )
