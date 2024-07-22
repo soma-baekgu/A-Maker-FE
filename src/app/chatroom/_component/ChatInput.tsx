c71e54348f5505fb10ffd21dcbae23a835fb5d9b
nohy6630 <nohy6630@gmail.com>
2024-07-22 17:50:10 +0900

    feat: 텍스트 채팅 구현

--- src/app/chatroom/_component/ChatInput.tsx
+++ src/app/chatroom/_component/ChatInput.tsx
@@ -1,22 +1,41 @@
+'use client';
+
 import styles from './chatInput.module.css';
 import Image from "next/image";
+import {useState} from "react";
 
 type Props = {
-    onSend: () => void
+    onSend: (msg:string) => void
 }
 
 export default function ChatInput({onSend}: Props) {
+    const [message, setMessage] = useState('');
 
     const onPlusClick = () => {
         console.log('plus');
+    }
+
+    const handleSend = () => {
+        onSend(message);
+        setMessage(''); // 메시지 전송 후 input 필드 초기화
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(event.target.value);
+    }
+
+    const handleKeyPress = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            handleSend();
+        }
     }
 
     return (
         <div className={styles.component}>
             <Image src="/button/plus.png" alt="plus" width={47} height={47} className={styles.plusButton}
                    onClick={onPlusClick}/>
-            <input className={styles.input} type="text" placeholder="채팅을 입력하세요."/>
-            <div className={styles.sendButton} onClick={onSend}>전송</div>
+            <input className={styles.input} type="text" placeholder="채팅을 입력하세요." onChange={handleChange} value={message} onKeyPress={handleKeyPress}/>
+            <div className={styles.sendButton} onClick={handleSend}>전송</div>
         </div>
     );
 }
