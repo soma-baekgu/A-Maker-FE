9194251c1f96eade5f534925b09baab58c293a98
nohy6630 <nohy6630@gmail.com>
2024-07-28 02:00:04 +0900

    feat: axios를 이용한 api 환경 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+'use client';
+
+import styles from './page.module.css';
+import Image from "next/image";
+import axios from "axios";
+
+export default function Page() {
+  const onClickLogin = async () => {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1/auth/oauth/google`);
+    window.location.href = response.data.data.url;
+  }
+
   return (
-      <>a-maker-fe</>
+      <div className={styles.page}>
+        <div className={styles.logo}>A-MAKER</div>
+        <div className={styles.button} onClick={onClickLogin}>
+          <Image src="/button/google.png" alt="google" width={58} height={54}/>
+          <div>Google 로그인</div>
+        </div>
+      </div>
   );
 }
