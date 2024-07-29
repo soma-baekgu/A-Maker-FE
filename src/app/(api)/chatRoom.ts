0436e0e9afb2a85a6a47cc2e48b4ce41cb0904a7
nohy6630 <nohy6630@gmail.com>
2024-07-29 14:59:29 +0900

    fix: 채팅방 참여 api 연동

--- src/app/(api)/chatRoom.ts
+++ src/app/(api)/chatRoom.ts
@@ -4,6 +4,7 @@ const chatRoomApi = {
     getListJoined: (workspaceId) => authRequest.get(`/api/v1/workspaces/${workspaceId}/chat-rooms/joined`),
     getListNotJoined: (workspaceId) => authRequest.get(`/api/v1/workspaces/${workspaceId}/chat-rooms/not-joined`),
     create: (workspaceId, chatroomName) => authRequest.post(`/api/v1/workspaces/${workspaceId}/chat-rooms`, {name: chatroomName}),
+    join:(workspaceId,chatRoomId) => authRequest.post(`/api/v1/workspaces/${workspaceId}/chat-rooms/${chatRoomId}/join`),
 };
 
 export default chatRoomApi;
