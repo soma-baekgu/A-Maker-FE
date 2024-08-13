ba2034b44f09eaf4e356d8bfe7543918d29722f9
nohy6630 <nohy6630@gmail.com>
2024-08-13 18:58:14 +0900

    fix: authCode 타입 추가

--- src/app/(api)/auth.ts
+++ src/app/(api)/auth.ts
@@ -2,7 +2,7 @@ import {request} from './config';
 
 const authApi = {
     oauthUrl: () => request.get(`/api/v1/auth/oauth/google`),
-    login: (authCode) => request.post(`/api/v1/auth/code/google?code=${authCode}`),
+    login: (authCode:string) => request.post(`/api/v1/auth/code/google?code=${authCode}`),
 };
 
 export default authApi;
