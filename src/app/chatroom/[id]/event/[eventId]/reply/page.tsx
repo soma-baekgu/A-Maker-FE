17d046b120a01ddfa14b6b102b7abbf344aee652
nohy6630 <nohy6630@gmail.com>
2024-08-13 13:25:57 +0900

    feat: 이벤트 상세 api 연동

--- src/app/chatroom/[id]/event/[eventId]/reply/page.tsx
+++ src/app/chatroom/[id]/event/[eventId]/reply/page.tsx
@@ -5,60 +5,57 @@ import styles from './page.module.css'
 import TopBar2 from "@/app/_component/TopBar2";
 import Profile from "@/app/chatroom/[id]/event/_component/Profile";
 import ReplyInput from "@/app/chatroom/[id]/event/_component/ReplyInput";
+import {useEffect, useState} from "react";
+import eventApi from "@/app/(api)/event";
 
-export default function Page() {
-    const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit'
-    };
+type Comment = {
+    img: string,
+    name: string,
+    time: Date,//todo: string으로 수정
+    content: string
+}
 
-    const dummy = {
-        "id": 1,
-        "eventTitle": "우리 어디서 만날지",
-        "eventDetails": "우리 어디서 만날지 정해봅시다",
-        "deadLine": "2024-07-28T14:54:43.526Z",
-        "notificationStartTime": "2024-07-28T14:54:43.527Z",
-        "notificationInterval": 15,
-        "eventCreator": {
-            "name": "이승환",
-            "email": "dltmd202@gmail.com",
-            "picture": "http://127.0.0.1/images/default_thumbnail.png"
-        },
-        "finishUser": [
-            {
-                "name": "이승환",
-                "email": "dltmd202@gmail.com",
-                "picture": "http://127.0.0.1/images/default_thumbnail.png"
-            },
-            {
-                "name": "노영진",
-                "email": "dltmd202@gmail.com",
-                "picture": "http://127.0.0.1/images/default_thumbnail.png"
-            }
-        ],
-        "waitingUser": [
-            {
-                "name": "이승환",
-                "email": "dltmd202@gmail.com",
-                "picture": "http://127.0.0.1/images/default_thumbnail.png"
-            },
-            {
-                "name": "허석문",
-                "email": "dltmd202@gmail.com",
-                "picture": "http://127.0.0.1/images/default_thumbnail.png"
-            }
-        ]
-    }
+type User = {
+    name: string,
+    email: string,
+    picture: string
+}
 
-    type Comment = {
-        img: string,
-        name: string,
-        time: Date,
-        content: string
-    }
+type EventData = {
+    id: number,
+    eventTitle: string,
+    eventDetails: string,
+    deadLine: string,
+    notificationStartTime: string,
+    notificationInterval: number,
+    eventCreator: User,
+    finishUser: User[],
+    waitingUser: User[]
+}
+
+const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+};
+
+export default function Page(props) {
+    const chatRoomId = props.params.id;
+    const eventId = props.params.eventId;
+    const [event, setEvent] = useState<EventData>();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await eventApi.readReplyEvent(chatRoomId, eventId);
+            setEvent(res.data.data);
+        }
+        fetchData().then(() => {
+            setIsLoaded(true);
+        });
+    }, []);
 
     const comments: Comment[] = [
         {
@@ -86,55 +83,64 @@ export default function Page() {
     return (
         <div className={styles.page}>
             <TopBar2 title="이벤트 상세"/>
-            <div className={styles.title}>
-                <Image src="/icon/reply.png" alt="reply" width={32} height={32}/>
-                <div className={styles.titleText}>{dummy.eventTitle}</div>
-            </div>
-            <div className={`${styles.section} ${styles.info}`}>
-                <div className={styles.value}>
-                    <div className={styles.description}>답변 대기중</div>
-                    <div className={styles.row}>
-                        {dummy.waitingUser.map((user, index) => (
-                            <Profile key={index} name={user.name} img={user.picture}/>
-                        ))}
+            {isLoaded && event ? (
+                <>
+                    <div className={styles.title}>
+                        <Image src="/icon/reply.png" alt="reply" width={32} height={32}/>
+                        <div className={styles.titleText}>{event.eventTitle}</div>
                     </div>
-                </div>
-                <div className={styles.value}>
-                    <div className={styles.description}>답변 완료</div>
-                    <div className={styles.row}>
-                        {dummy.finishUser.map((user, index) => (
-                            <Profile key={index} name={user.name} img={user.picture}/>
-                        ))}
-                    </div>
-                </div>
-
-                <div className={styles.value}>
-                    <div className={styles.description}>마감 기한</div>
-                    <div className={styles.row}>{new Date(dummy.deadLine).toLocaleTimeString('ko-KR', options)}</div>
-                </div>
-
-                <div className={styles.value}>
-                    <div className={styles.description}>이벤트 생성자</div>
-                    <div className={styles.row}>
-                        <Profile name={dummy.eventCreator.name} img={dummy.eventCreator.picture}/>
-                    </div>
-                </div>
-
-            </div>
-            <div className={`${styles.detailText} ${styles.section}`}>{dummy.eventDetails}</div>
-            <div className={styles.comments}>
-                {
-                    comments.map((comment, index) => (
-                        <div key={index} className={`${styles.section} ${styles.comment}`}>
-                            <div className={styles.commentTitle}>
-                                <Profile name={comment.name} img={comment.img}/>
-                                <div className={styles.time}>{timeAgo(comment.time)}</div>
+                    <div className={`${styles.section} ${styles.info}`}>
+                        <div className={styles.value}>
+                            <div className={styles.description}>답변 대기중</div>
+                            <div className={styles.row}>
+                                {event.waitingUser?.map((user, index) => (
+                                    <Profile key={index} name={user.name} img={user.picture}/>
+                                ))}
                             </div>
-                            <div className={styles.content}>{comment.content}</div>
                         </div>
-                    ))
-                }
-            </div>
+                        <div className={styles.value}>
+                            <div className={styles.description}>답변 완료</div>
+                            <div className={styles.row}>
+                                {event.finishUser?.map((user, index) => (
+                                    <Profile key={index} name={user.name} img={user.picture}/>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className={styles.value}>
+                            <div className={styles.description}>마감 기한</div>
+                            <div
+                                className={styles.row}>{new Date(event.deadLine).toLocaleTimeString('ko-KR', options)}</div>
+                        </div>
+
+                        <div className={styles.value}>
+                            <div className={styles.description}>이벤트 생성자</div>
+                            <div className={styles.row}>
+                                {event.eventCreator &&
+                                    <Profile name={event.eventCreator.name} img={event.eventCreator.picture}/>
+                                }
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className={`${styles.detailText} ${styles.section}`}>{event.eventDetails}</div>
+                    <div className={styles.comments}>
+                        {
+                            comments.map((comment, index) => (
+                                <div key={index} className={`${styles.section} ${styles.comment}`}>
+                                    <div className={styles.commentTitle}>
+                                        <Profile name={comment.name} img={comment.img}/>
+                                        <div className={styles.time}>{timeAgo(comment.time)}</div>
+                                    </div>
+                                    <div className={styles.content}>{comment.content}</div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
+            ) : (
+                <div>Loading...</div>
+            )}
             <ReplyInput onSend={(msg: string) => {
                 console.log(msg);
             }}/>
