014180d650f7614718b62bc11f76ff5726f4e355
nohy6630 <nohy6630@gmail.com>
2024-08-13 19:01:05 +0900

    fix: chat.ts 타입 추가

--- src/app/(api)/chat.ts
+++ src/app/(api)/chat.ts
@@ -1,12 +1,12 @@
 import {authRequest} from "@/app/(api)/config";
 
 const chatApi = {
-    send: (chatRoomId, content) => authRequest.post(`/api/v1/chat-rooms/${chatRoomId}/chats`, {content}),
-    sendImg: (chatRoomId, path) => authRequest.post(`/api/v1/chat-rooms/${chatRoomId}/chats/img`, {path}),
-    sendFile: (chatRoomId, path) => authRequest.post(`/api/v1/chat-rooms/${chatRoomId}/chats/file`, {path}),
-    recentChat: (chatRoomId) => authRequest.get(`/api/v1/chat-rooms/${chatRoomId}/chats/recent`),
-    previousChat: (chatRoomId, cursor, size) => authRequest.get(`/api/v1/chat-rooms/${chatRoomId}/chats/previous?cursor=${cursor}&size=${size}`),
-    afterChat: (chatRoomId, cursor, size) => authRequest.get(`/api/v1/chat-rooms/${chatRoomId}/chats/after?cursor=${cursor}&size=${size}`),
+    send: (chatRoomId:number, content:string) => authRequest.post(`/api/v1/chat-rooms/${chatRoomId}/chats`, {content}),
+    sendImg: (chatRoomId:number, path:string) => authRequest.post(`/api/v1/chat-rooms/${chatRoomId}/chats/img`, {path}),
+    sendFile: (chatRoomId:number, path:string) => authRequest.post(`/api/v1/chat-rooms/${chatRoomId}/chats/file`, {path}),
+    recentChat: (chatRoomId:number) => authRequest.get(`/api/v1/chat-rooms/${chatRoomId}/chats/recent`),
+    previousChat: (chatRoomId:number, cursor:number, size:number) => authRequest.get(`/api/v1/chat-rooms/${chatRoomId}/chats/previous?cursor=${cursor}&size=${size}`),
+    afterChat: (chatRoomId:number, cursor:number, size:number) => authRequest.get(`/api/v1/chat-rooms/${chatRoomId}/chats/after?cursor=${cursor}&size=${size}`),
 };
 
 export default chatApi;
