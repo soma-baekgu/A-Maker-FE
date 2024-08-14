4567d4ca8e5c613b3f82439d9b9989d2f2648343
nohy6630 <nohy6630@gmail.com>
2024-08-14 12:10:30 +0900

    fix: 초대페이지 워크스페이스 정보 반영

--- src/app/invite/[workspaceId]/page.tsx
+++ src/app/invite/[workspaceId]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import styles from './page.module.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Image from "next/image";
 import workspaceApi from "@/app/(api)/workspace";
 import {useRouter} from "next/navigation";
@@ -9,7 +9,7 @@ import {useRouter} from "next/navigation";
 type workspaceInfo = {
     id: number,
     name: string,
-    imgUrl: string,
+    thumbnail: string,
 }
 
 type Props = {
@@ -19,15 +19,21 @@ type Props = {
 }
 
 export default function Page(props: Props) {
-    const [workspaceInfo, setWorkspaceInfo] = useState<workspaceInfo>({
-        id: 1,
-        name: '캡스톤 디자인 2조',
-        imgUrl: '/images/default_thumbnail.png'
-    });
+    const [workspaceInfo, setWorkspaceInfo] = useState<workspaceInfo | null>(null);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     const workspaceId = Number(props.params.workspaceId);
 
     const router = useRouter();
+
+    useEffect(() => {
+        fetchWorkspace().then(() => setIsLoaded(true));
+    }, []);
+
+    const fetchWorkspace = async () => {
+        const res = await workspaceApi.get(workspaceId);
+        setWorkspaceInfo(res.data.data);
+    }
 
     const handleApprove = () => {
         workspaceApi.approve(workspaceId).then(() => {
@@ -37,15 +43,24 @@ export default function Page(props: Props) {
 
     return (
         <div className={styles.page}>
-            <div className={styles.title}>
-                <Image className={styles.image} src={workspaceInfo.imgUrl} alt="img" width={60} height={60}/>
-                <div>{workspaceInfo.name}</div>
-            </div>
-            <div className={styles.help}>
-                워크스페이스에 초대되었습니다.<br/>
-                버튼을 클릭해 워크스페이스에 참가해보세요!
-            </div>
-            <div className={styles.button} onClick={handleApprove}>워크스페이스 참가</div>
+            {isLoaded ?
+                <>
+                    <div className={styles.title}>
+                        <Image className={styles.image} src={workspaceInfo!.thumbnail} alt="img" width={60}
+                               height={60}/>
+                        <div>{workspaceInfo!.name}</div>
+                    </div>
+                    <div className={styles.help}>
+                        워크스페이스에 초대되었습니다.<br/>
+                        버튼을 클릭해 워크스페이스에 참가해보세요!
+                    </div>
+                    <div className={styles.button} onClick={handleApprove}>워크스페이스 참가</div>
+                </>
+                :
+                <>
+                    Loading...
+                </>
+            }
         </div>
     )
 }
