a33f9290d76535748ee958a3ed1f49d407b090b6
nohy6630 <nohy6630@gmail.com>
2024-09-04 12:40:38 +0900

    feat: default 페이지 디자인 적용

--- src/app/default/page.tsx
+++ src/app/default/page.tsx
@@ -5,6 +5,7 @@ import React, {useState} from "react";
 import workspaceApi from "@/app/(api)/workspace";
 import CreateWorkspaceModal from "@/app/_component/CreateWorkspaceModal";
 import {useRouter} from "next/navigation";
+import Image from "next/image";
 
 export default function Page() {
     const [modalVisible, setModalVisible] = useState(false);
@@ -18,11 +19,16 @@ export default function Page() {
 
     return (
         <div className={styles.page}>
+            <Image src={"/login/pattern2.png"} alt={"pattern"}
+                   className={styles.pattern}
+                   width={390} height={844}/>
             <div className={styles.help}>
-                워크스페이스를 만들어서 팀원들을 초대해보세요!
+                <div className={styles.helpText1}>Let&apos;s start!</div>
+                <div className={styles.helpText2}>워크스페이스를 만들어서<br/>팀원들을 초대해보세요!</div>
             </div>
             <div className={styles.button} onClick={()=>{setModalVisible(true)}}>
-                새로운 워크스페이스
+                <Image src={"/login/plus.png"} alt={"plus"} width={25} height={24}/>
+                <div>새로운 워크스페이스</div>
             </div>
             {modalVisible &&
                 <CreateWorkspaceModal setVisible={setModalVisible} createWorkspace={createWorkspace}/>}
