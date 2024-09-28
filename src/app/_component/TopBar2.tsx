6e0d3ff0e27a61743e117c53f49199161d858112
nohy6630 <nohy6630@gmail.com>
2024-09-28 13:10:50 +0900

    fix: 배경색 수정

--- src/app/_component/TopBar2.tsx
+++ src/app/_component/TopBar2.tsx
@@ -15,7 +15,7 @@ export default function TopBar2({title}: Props) {
 
     return (
         <div className={styles.component}>
-            <Image className={styles.backButton} src="/topbar/back.png" alt="back" width={24} height={24}
+            <Image className={styles.backButton} src="/topBar/back.png" alt="back" width={24} height={24}
                    onClick={onBack}/>
             <div className={styles.title}>{title}</div>
         </div>
