76401c50f56c962415f429160ff382928a948537
nohy6630 <nohy6630@gmail.com>
2024-11-09 17:22:43 +0900

    feat: 리액션 이벤트 응답 생성 api 연동

--- src/app/(api)/eventComment.ts
+++ src/app/(api)/eventComment.ts
@@ -5,6 +5,7 @@ const eventCommentApi = {
     readReplyComment: (eventId: number, page: number, size: number) => authRequest.get(`/api/v1/events/${eventId}/reply/comments`, {
         params: {page, size}
     }),
+    createReactionComment: (eventId: number, optionId: number) => authRequest.post(`/api/v1/events/${eventId}/reaction/comments`, {optionId}),
 }
 
 export default eventCommentApi;
--- src/app/chatroom/[id]/event/[eventId]/reaction/page.tsx
+++ src/app/chatroom/[id]/event/[eventId]/reaction/page.tsx
@@ -6,11 +6,12 @@ import {useEffect, useState} from "react";
 import {EventData, User} from "@/app/chatroom/[id]/event/[eventId]/types";
 import EventInfo from "@/app/chatroom/[id]/event/[eventId]/_component/EventInfo";
 import eventApi from "@/app/(api)/event";
+import eventCommentApi from "@/app/(api)/eventComment";
 
-interface Option{
-    id:number,
-    eventId:number,
-    content:string
+interface Option {
+    id: number,
+    eventId: number,
+    content: string
 }
 
 interface ReactionEventData extends EventData {
@@ -25,16 +26,23 @@ export default function Page(props: {
 }) {
     const chatRoomId: number = Number(props.params.id);
     const eventId: number = Number(props.params.eventId);
-    const dummyUser: User = {
-        name: "노영진",
-        email: "shane9747@gmail.com",
-        picture: "https://lh3.googleusercontent.com/a/ACg8ocKoltqSQEeJytHSjnxp7xMKzStDF9KkwCBFYZgLEUmqXF-Khg=s96-c"
-    }
     const [event, setEvent] = useState<ReactionEventData>();
     const [isLoaded, setIsLoaded] = useState(false);
+    const [selectedOption, setSelectedOption] = useState<number>();
+
     const loadEventData = async () => {
-        const res = await eventApi.readReactionEvent(chatRoomId, eventId)
-        setEvent(res.data.data)
+        const res = await eventApi.readReactionEvent(chatRoomId, eventId);
+        setEvent(res.data.data);
+    }
+
+    const createEventComment = async (optionId: number | undefined) => {
+        if (!optionId) return;
+        await eventCommentApi.createReactionComment(eventId, optionId);
+        //todo event comment 조회 api 호출
+    }
+
+    const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedOption(Number(e.target.value));
     }
 
     useEffect(() => {
@@ -58,12 +66,22 @@ export default function Page(props: {
                     <div className={styles.choice}>
                         {event.options.map((option, index) => (
                             <div className={styles.item} key={index}>
-                                <input className={styles.checkbox} type="radio" name={"vote"}/>
+                                <input
+                                    className={styles.checkbox}
+                                    type="radio"
+                                    name={"vote"}
+                                    value={option.id}
+                                    onChange={handleOptionChange}
+                                />
                                 {option.content}
                             </div>
                         ))}
                     </div>
-                    <div className={styles.button}>응답</div>
+                    <div className={selectedOption ? styles.button : styles.disableButton} onClick={() => {
+                        if (!selectedOption) return;
+                        createEventComment(selectedOption);
+                    }}>응답
+                    </div>
                     <div className={styles.empty}></div>
                 </div>
             ) : (
